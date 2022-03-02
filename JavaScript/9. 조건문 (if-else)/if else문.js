69686513134f0ae92bb97efebf62ac0610897fb2
Phantester S <67867993+Haruster@users.noreply.github.com>
2022-03-03 00:57:41 +0900

    master

---
+++ JavaScript/9. 조건문 (if-else)/if else문.js
@@ -0,0 +1,19 @@
+const age = 20;
+
+if (age < 20) {
+
+    console.log("age는 20보다 작습니다.");
+
+} else if (age == 20) {
+
+    console.log("age는 20입니다.");
+
+} else if (age > 20) {
+
+    console.log("age는 20보다 큽니다.");
+
+} else {
+
+    console.log("값이 올바르지 않습니다.")
+
+}
---
+++ JavaScript/9. 조건문 (if-else)/중첩 if문.js
@@ -0,0 +1,23 @@
+const age = 20;
+
+if (age <= 100) {
+
+    console.log("age는 100보다 작거나 같습니다.");
+
+    if (age <= 20) {
+
+        console.log("age는 20보다 작거나 같습니다.");
+
+    }
+
+} else if (age => 100) {
+
+    console.log("age는 100보다 크거나 같다.");
+
+    if (age > 100) {
+
+        console.log("age는 100보다 크다.");
+
+    }
+
+}
