dcda8e9e7ce6479a3bd0a77bc14bcbc7533f6467
Phantester S <67867993+Haruster@users.noreply.github.com>
2022-03-03 04:06:51 +0900

    master

---
+++ JavaScript/11. while문/while문.js
@@ -0,0 +1,9 @@
+const setVar = 1000;
+
+var userVar = 1;
+
+while (userVar == setVar) {
+
+    console.log("현재 userVar의 값은 ", userVar, "입니다.")
+
+}
---
+++ JavaScript/12. do~while문/do~while.js
@@ -0,0 +1,11 @@
+const setvar = 10000;
+
+var userVar = 10;
+
+do {
+
+    console.log("userVar의 값은 ", userVar);
+
+    userVar++;
+
+} while (userVar == setvar);
