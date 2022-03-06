6a6933172d3d249d9e2445e99c92a8cdc1b9d36d
Phantester S <67867993+Haruster@users.noreply.github.com>
2022-03-07 04:06:05 +0900

    master

---
+++ JavaScript/5. alert, prompt, confirm/confirm() 사용.js
@@ -0,0 +1,5 @@
+// confirm() 사용
+
+var userinput = confirm("사용자 이름이 haruster가 맞습니까?");
+
+console.log(userinput);
--- JavaScript/5. alert, prompt, confirm/prompt() 사용(디폴트 값 사용).js
+++ JavaScript/5. alert, prompt, confirm/prompt() 사용(디폴트 값 사용).js
@@ -1,1 +1,6 @@
-// con
+// prompt() 사용(디폴트값 사용)
+
+var userinput = prompt("이름을 입력해주세요" + "haruster");
+
+alert(userinput);
+console.log(userinput);
