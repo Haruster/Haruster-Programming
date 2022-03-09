8556e4a2f4244f7aba8978a34bb2a6d9547955d7
Phantester S <67867993+Haruster@users.noreply.github.com>
2022-03-10 00:47:09 +0900

    master

---
+++ JavaScript/7. 기본 연산자/증감연산자.js
@@ -0,0 +1,9 @@
+var number1 = 10;
+var number2 = 20;
+var number3 = 30;
+var number4 = 40;
+
+console.log("선증가한 데이터를 출력합니다.", ++number1);
+console.log("선감소한 데이터를 출력합니다.", --number2);
+console.log("후증가한 데이터를 출력합니다.", number3++);
+console.log("후감소한 데이터를 출력합니다.", number4--);
---
+++ JavaScript/7. 기본 연산자/축약 연산자.js
@@ -0,0 +1,17 @@
+var number1 = 10;
+var number2 = 20;
+var number3 = 30;
+var number4 = 40;
+var number5 = 50;
+
+number1 += 10;
+number2 -= 10;
+number3 *= 400;
+number4 /= 10;
+number5 %= 2
+
+console.log(`더하기의 결과는 ${number1}입니다.`);
+console.log(`빼기의 결과는 ${number2}입니다.`);
+console.log(`곱하기의 결과는 ${number3}입니다.`);
+console.log(`나누기의 결과는 ${number4}입니다.`);
+console.log(`나머지는 ${number5}입니다.`);
