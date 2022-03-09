47d7240d2b5d673cdcd0fe422047582a640ede24
Phantester S <67867993+Haruster@users.noreply.github.com>
2022-03-10 00:51:38 +0900

    master

---
+++ JavaScript/7. 기본 연산자/비교연산자.js
@@ -0,0 +1,14 @@
+// 비교연산자는 값이 올바르면 true, 틀리면 false를 반환한다.
+
+var number1 = 30;
+var number2 = 20
+var number3 = 35;
+var number4 = 30;
+
+console.log(number1 < number2);
+console.log(number2 > number1);
+console.log(number1 <= number2);
+console.log(number4 >= number1);
+console.log(number1 == number4);
+console.log(number1 != number2);
+console.log(number1 === number2); // 타입 비교
