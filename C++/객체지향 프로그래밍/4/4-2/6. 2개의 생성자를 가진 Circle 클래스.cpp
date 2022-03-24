193a81bd7517836bacbffcdf4d6f47b8ebf2bf44
Phantester S <67867993+Haruster@users.noreply.github.com>
2022-03-24 23:13:16 +0900

    master

--- C++/객체지향 프로그래밍/4/4-2/6. 2개의 생성자를 가진 Circle 클래스.cpp
+++ C++/객체지향 프로그래밍/4/4-2/6. 2개의 생성자를 가진 Circle 클래스.cpp
@@ -40,6 +40,16 @@ int main() {
 
     Circle donut; // 매개변수 없는 생성자 호출
 
-    double area = donut.getArea(); //
+    double area = donut.getArea(); 
+
+    cout << "donut의 면적은 " << area << endl;
+
+    Circle pizza(30); // 매개 변수 있는 생성자 호출
+
+    area = pizza.getArea();
+
+    cout << "pizza의 면적은 " << area << endl;
+
+    return 0;
 
 }
