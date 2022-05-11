dcdebdc758ceee5fe087a021239f5c3f6095a604
Phantester S <67867993+Haruster@users.noreply.github.com>
2022-05-11 11:35:38 +0900

    master

--- 자료구조/11/0/2. 링크 표현법 프로그램.c
+++ 자료구조/11/0/2. 링크 표현법 프로그램.c
@@ -22,6 +22,21 @@ void main() {
     n2 = (TreeNode *)malloc(sizeof(TreeNode));
     n3 = (TreeNode *)malloc(sizeof(TreeNode));
 
+    n1->data = 10;  // 첫 번째 노드
+    n1->left = n2;
+    n1->right = n3;
+
+    n2->data = 20;  // 두 번째 노드
+    n2->left = NULL;
+    n2->right = NULL;
+
+    n3->data = 30;  // 세 번째 노드
+    n3->left = NULL;
+    n3->right = NULL;
+
+    free(n1); free(n2); free(n3);
+
+    return 0;
 
 
 }
