7dfcdc67f71ccf7201e295689f92b7a3018e15ae
Phantester S <67867993+Haruster@users.noreply.github.com>
2022-05-24 23:09:48 +0900

    master

--- 자료구조/12/12-1/6. 허프만 코드 프로그램.c
+++ 자료구조/12/12-1/6. 허프만 코드 프로그램.c
@@ -56,6 +56,28 @@ void init(HeapType* h) {
 
 void insert_min_heap(HeapType* h, element item) {
 
+    int i;
+
+    i = ++(h->heap_size);
+
+    // 트리를 거슬러 올라가면서 부모 노드와 비교하는 과정
+
+    while ((i != 1) && (item.key < h->heap[i / 2].key)) {
+
+        h->heap[i] = h->heap[i/2];
+
+        i /= 2;
+
+    }
+
+    h->heap[i] = item;  // 새로운 노드를 삽입
+
+}
+
+// 삭제 함수
+
+element delete_min_heap(HeapType* h) {
+
     
 
 }
